2e03c68b42d0bbe834f82ed7e3f274df70ec67c2
wayhuay <wayhuay@users.noreply.github.com>
2015-08-09 01:37:10 +0800

    Module 4 Project 1 - Plot 1.R

---
+++ Plot 1.R
@@ -0,0 +1,27 @@
+# loading data into R
+data <- read.table("household_power_consumption.txt", 
+                   sep = ';', 
+                   header = TRUE, 
+                   na.string = '?')
+
+
+# time & date formatting & subsetting the required dates
+data$Time <- strptime(data$Time, format = "%d/%m/%Y %H:%M:%S")
+data$Date <- as.Date(data$Date,format = "%d/%m/%Y")
+data_subset <- data[data$Date <= "2007-02-02" & data$Date >= "2007-02-01",]
+
+
+# create a png file
+png(filename="plot1.png", width = 480, height = 480, bg = "transparent")
+
+
+# plot histogram
+hist(data_subset$Global_active_power,
+     col = "red",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency",
+     main = "Global Active Power")
+
+# save the png file
+dev.off()
+
